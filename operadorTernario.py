ca62b04a3a01162b232ead464134cdb37a975fd5
Lucas Aprigio <lucsaprigio@hotmail.com>
2021-11-13 19:23:27 -0300

    Operador Ternário

---
+++ operadorTernario.py
@@ -0,0 +1,16 @@
+# Caso a idade seja maior ou igual a 18 anos, essa pessoa é maior de idade
+# Caso o contrário ela é menor de idade
+idade = 15
+if idade >= 18:
+    print('Maior de Idade')
+else:
+    print('Menor de idade')
+
+# Operador Ternário
+idade = 25
+print('Maior de idade') if idade >= 18 else print('Menor de Idade')
+# expressão if condição else expressão
+
+# Desafio
+velocidade = 80
+print('Você foi multado') if velocidade >= 100 else print('Siga em frente')
