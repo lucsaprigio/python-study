4ad9c39ced55872431da3595dbea0fe95cf09000
Lucas Aprigio <lucsaprigio@hotmail.com>
2021-11-13 14:54:18 -0300

    Operations

---
+++ app.py
@@ -0,0 +1,19 @@
+# Tipos de números que podem ser usados no Python
+a = 20
+b = 20.5
+
+print(type(a)) # Int
+print(type(b)) # Float
+
+# Operações Matemáticas
+print(10 + 6)
+print(10 - 6)
+print(10 * 6)
+print(10 / 6)
+print(10 // 6) # divisão de inteiro, quantas vezes podem ser divididos
+print(10 % 6) # resto da divisão
+print(10 ** 6) # exponenciais
+
+import math
+print(round(5.7)) # Arredonda o mais próximo no caso 6
+print(math.ceil(2.2)) # Arredonda para cima no caso 3
