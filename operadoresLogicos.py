6c82c95d07bcad7f8df1eeb13b2ac4f8cf982435
Lucas Aprigio <lucsaprigio@hotmail.com>
2021-11-13 19:21:47 -0300

    Operadores Lógicos

---
+++ operadoresLogicos.py
@@ -0,0 +1,13 @@
+# Operadores Lógicos
+idade = 15
+print(idade > 17) # Maior que
+print(idade < 17) # Menor que
+print(idade <= 17) # Menor ou igual que
+print(idade >= 17) # Maior ou igual que
+print(idade == 17) # igual
+print(idade != 17) # Diferente de
+# Comparações entre outros tupos
+print(True == True)
+print('Rafael' == 'rafael') 
+
+""" Python é Case sensitive, então ele compara letrar minúsculas e maiúsculas, faz diferença! """
