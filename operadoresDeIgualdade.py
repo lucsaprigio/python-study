2eaa88cf082dbaef51cf0f5364ef010dec3fde7f
Lucas Aprigio <lucsaprigio@hotmail.com>
2021-11-13 19:22:58 -0300

    Operadores de Igualdade

---
+++ operadoresDeIgualdade.py
@@ -0,0 +1,13 @@
+# Operadores de Igualdade
+a = 'Python'
+b = 'Python'
+print(a == b)
+print(a is b)
+
+# == compara o valor
+# is compara a identidade
+
+c = 10 
+d = 10
+print(c == d)
+print(c is d)
