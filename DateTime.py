97b983ac7136eca669b52ce91d44c75a6131c43f
Lucas Aprigio <lucsaprigio@hotmail.com>
2021-11-13 14:55:33 -0300

    DateTimeC:/Users/Aprigio/AppData/Local/Programs/Python/Python39/python.exe c:/Users/Aprigio/Projects/Modulo02/DateTime.py

---
+++ DateTime.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+
+# Pega a data atual
+print(datetime.now())
+
+# Passando a propriedade abaixo, pega o Dia, Mês ou ano
+print(datetime.now().day)
+print(datetime.now().month)
+print(datetime.now().year)
