5d8b28d92f8631a952fcadcd561c8c69c71a06fd
Lucas Aprigio <lucsaprigio@hotmail.com>
2021-11-13 19:22:37 -0300

    Convertendo entre tipos

---
+++ convertendoEntreTipos.py
@@ -0,0 +1,18 @@
+# Conversão de tipos
+idade = input('Qual é a sua idade?')
+print(int(idade) > 18)
+
+print(type(str(5)))
+
+# Receber valores decimais
+altura_da_parede = input('Qual é a altura da parede')
+print(float(altura_da_parede) > 2.10)
+
+
+# Podemos converter para os valores abaixo.
+str()
+int()
+float()
+list()
+tuple()
+dict()
